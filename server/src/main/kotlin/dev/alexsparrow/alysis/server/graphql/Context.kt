db9062e2adcc6289e66edb9de9bd7ac1f972c4a9
Alex Sparrow <alex@alexsparrow.dev>
2020-07-23 15:37:32 +0100

    Plug authentication through context

---
+++ server/src/main/kotlin/dev/alexsparrow/alysis/server/graphql/Context.kt
@@ -0,0 +1,6 @@
+package dev.alexsparrow.alysis.server.graphql
+
+import com.expediagroup.graphql.execution.GraphQLContext
+import io.micronaut.security.authentication.Authentication
+
+data class Context(val authentication: Authentication?): GraphQLContext
---
+++ server/src/main/kotlin/dev/alexsparrow/alysis/server/graphql/GraphQLExecutionInput.kt
@@ -0,0 +1,25 @@
+package dev.alexsparrow.alysis.server.graphql
+
+import graphql.ExecutionInput
+import io.micronaut.configuration.graphql.DefaultGraphQLExecutionInputCustomizer
+import io.micronaut.configuration.graphql.GraphQLExecutionInputCustomizer
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.core.async.publisher.Publishers
+import io.micronaut.http.HttpRequest
+import io.micronaut.security.utils.SecurityService
+import org.reactivestreams.Publisher
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+@Replaces(DefaultGraphQLExecutionInputCustomizer::class)
+class GraphQLExecutionInput : GraphQLExecutionInputCustomizer {
+    @Inject
+    lateinit var securityService: SecurityService
+
+    override fun customize(executionInput: ExecutionInput, httpRequest: HttpRequest<*>?): Publisher<ExecutionInput> {
+        return Publishers.just(executionInput.transform { builder ->
+            builder.context(Context(securityService.authentication.orElse(null)))
+        })
+    }
+}
