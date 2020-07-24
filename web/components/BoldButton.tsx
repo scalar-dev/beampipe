b4b836809f3fbd2c66b54cc443fe7db5ba6c4832
Alex Sparrow <alex@alexsparrow.dev>
2020-07-24 10:49:03 +0100

    Green login button

--- web/components/BoldButton.tsx
+++ web/components/BoldButton.tsx
@@ -35,7 +35,7 @@ export const BoldButton = forwardRef<HTMLAnchorElement, AnchorProps>(
       {...otherProps}
       ref={ref}
       href={href}
-      className="inline-flex items-center justify-center px-5 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+      className="inline-flex items-center justify-center px-5 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
     >
       {children}
     </a>
