dc17322b0e53740f65e0a222cba0c1feb6ff9562
Alex Sparrow <alex@alexsparrow.dev>
2020-07-24 13:00:40 +0100

    Allow copy of script code

--- web/components/NonIdealState.tsx
+++ web/components/NonIdealState.tsx
@@ -1,17 +1,21 @@
+import { ReactNode } from "react";
+
 interface NonIdealStateProps {
   isIdeal: boolean;
+  nonIdeal?: ReactNode;
 }
 
 export const NonIdealState: React.FunctionComponent<NonIdealStateProps> = ({
   isIdeal,
   children,
+  nonIdeal = <div className="text-xl text-gray-500">No data to display</div>,
 }) => (
   <>
     {isIdeal ? (
       <>{children}</>
     ) : (
-      <div className="w-full h-full flex">
-        <div className="text-xl text-gray-500 m-auto">No data to display</div>
+      <div className="w-full max-w-full h-full flex">
+        <div className="m-auto max-w-full">{nonIdeal}</div>
       </div>
     )}
   </>
