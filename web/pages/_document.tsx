d3db467366fa0a832e83e1b4d5d9d826a0f98b5a
Alex Sparrow <alex@alexsparrow.dev>
2020-07-30 14:54:41 +0100

    favicon

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -5,7 +5,33 @@ class MyDocument extends Document {
       return (
         <Html>
           <Head>
-            <script async defer src="/tracker.js" data-beampipe-domain="beampipe.io" />
+            <script
+              async
+              defer
+              src="/tracker.js"
+              data-beampipe-domain="beampipe.io"
+            />
+
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
           </Head>
           <body>
             <Main />
