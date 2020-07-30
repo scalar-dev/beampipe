315e0c1e938de5eff2bb21b01b6065bcb0619cb7
Alex Sparrow <alex@alexsparrow.dev>
2020-07-30 07:28:05 +0100

    Nicer landing page

--- web/components/Layout.tsx
+++ web/components/Layout.tsx
@@ -23,7 +23,7 @@ export const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
       <div className="min-h-screen bg-gray-100">
         <div className="container m-auto">
           <nav className="flex items-center justify-between flex-wrap py-4">
-            <div className="flex items-center flex-shrink-0 text-black mr-6">
+            <div className="flex flex-grow text-black mr-6">
               <span className="font-extrabold text-3xl tracking-tight align-middle">
                 <Link href="/">
                   <a>
@@ -38,7 +38,7 @@ export const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
               </span>
             </div>
          
-            <div className="block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="block lg:flex lg:items-center lg:w-auto">
               <div className="text-sm lg:flex-grow"></div>
               <div>
                 {user ? (
