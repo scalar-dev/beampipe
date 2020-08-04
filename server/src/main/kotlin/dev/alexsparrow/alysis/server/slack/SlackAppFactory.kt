2c33b243c40ff97962b0048b27e28c1af0545837
Alex Sparrow <alex@alexsparrow.dev>
2020-08-04 21:17:45 +0100

    Ensure token is not null

--- server/src/main/kotlin/dev/alexsparrow/alysis/server/slack/SlackAppFactory.kt
+++ server/src/main/kotlin/dev/alexsparrow/alysis/server/slack/SlackAppFactory.kt
@@ -27,7 +27,6 @@ class AppFactory {
 
         app.command("/beampipe") { req, ctx ->
             val parts = req.payload.text.split("\\s+".toRegex())
-            println(parts)
 
             if (parts[0] == "subscribe") {
                 val domain = parts[1]
@@ -56,7 +55,7 @@ class AppFactory {
                     }
                 }
             } else {
-                ctx.ack("Unrecognised command")
+                ctx.ack("Usage: /beampipe subscribe <domain> <event>")
             }
         }
 
