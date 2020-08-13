35a6d966d7b0242f2dfa9a514291015d31c020b6
Alex Sparrow <alex@alexsparrow.dev>
2020-08-13 13:08:09 +0100

    Privacy page and footer

--- web/components/Laptop.tsx
+++ web/components/Laptop.tsx
@@ -1,7 +1,15 @@
 import s from "./Laptop.module.css";
 
 export const Laptop = () => (
-  <div className="w-full py-12">
+  <div
+    className="mx-auto"
+    style={{
+      width: "100%",
+      maxWidth: "48rem",
+      maxHeight: "calc(48rem * 3 / 5)",
+      height: "calc(100vw * 6 / 10)",
+    }}
+  >
     <div className={`${s.laptop} m-auto`}>
       <div className={s.upper}></div>
       <div className={s.lower}></div>
