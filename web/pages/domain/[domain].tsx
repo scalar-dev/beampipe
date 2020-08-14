dfb85bb1c71651385a175ff04ec070f12fbc38fc
Alex Sparrow <alex@alexsparrow.dev>
2020-08-14 13:44:50 +0100

    Add stats to domain page

--- web/pages/domain/[domain].tsx
+++ web/pages/domain/[domain].tsx
@@ -13,10 +13,7 @@ import { AuthProvider, UserContext } from "../../utils/auth";
 import { Menu, MenuSection, MenuItem } from "../../components/Menu";
 import { Tick } from "../../components/Tick";
 import { Domain } from "../../interfaces";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import numeral from "numeral";
-import { Spinner } from "../../components/Spinner";
+import { Stats } from "../../components/viz/Stats";
 
 const cardHeight = "27rem";
 
@@ -121,53 +118,6 @@ const TimePicker = ({
     </Menu>
   );
 };
-
-const PercentageChange: React.FunctionComponent<{
-  current: number;
-  previous: number;
-}> = ({ current, previous }) => {
-  const change = previous > 0 ? (current - previous) / previous : 0;
-
-  return (
-    <div className="text-sm">
-      <FontAwesomeIcon
-        size="sm"
-        className={`fill-current w-4 h-4 mr-2 ${
-          change >= 0 ? "text-green-600" : "text-red-600"
-        }`}
-        icon={change >= 0 ? faArrowUp : faArrowDown}
-      />
-      {numeral(Math.abs(change)).format("0%")}
-    </div>
-  );
-};
-
-const Stats = ({ stats }: { stats?: any }) => (
-  <div>
-    {stats ? (
-      <div className="flex flex-row">
-        <div className="text-2xl mr-4">
-          <span className="text-gray-500 mr-2 text-sm">total</span>
-          {numeral(stats.count).format("0.[0]a")}
-          <PercentageChange
-            current={stats.count}
-            previous={stats.previousCount}
-          />
-        </div>
-        <div className="text-2xl">
-          <span className="text-gray-500 mr-2 text-sm">unique</span>
-          {numeral(stats.countUnique).format("0.[0]a")}
-          <PercentageChange
-            current={stats.countUnique}
-            previous={stats.previousCountUnique}
-          />
-        </div>
-      </div>
-    ) : (
-      <Spinner />
-    )}
-  </div>
-);
 
 const Root: React.FunctionComponent<{ domain: string }> = ({ domain }) => {
   const [timePeriod, setTimePeriod] = useState("day");
