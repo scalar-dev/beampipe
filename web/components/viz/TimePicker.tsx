7785032450dcc6ca07582b8b7327d1b2349312da
Alex Sparrow <alex@alexsparrow.dev>
2020-08-18 20:58:34 +0100

    Custom time range picker

--- web/components/viz/TimePicker.tsx
+++ web/components/viz/TimePicker.tsx
@@ -1,24 +1,48 @@
-import { useState } from "react";
-import { Menu, MenuSection, MenuItem } from "../Menu";
+import { useState, useEffect } from "react";
+import { Menu, MenuSection, MenuItem, MenuDivider } from "../Menu";
 import { Tick } from "../marketing/Tick";
+import dynamic from "next/dynamic";
+import moment from "moment";
 
-const timePeriods = ["hour", "day", "week", "month"];
+type TimePeriodType = "hour" | "day" | "week" | "month" | "custom";
 
-const displayTimePeriod = (timePeriod: string) => {
-  if (timePeriod == "hour") return "Last hour";
-  else if (timePeriod == "day") return "Last 24 hours";
-  else if (timePeriod == "week") return "Last 7 days";
-  else if (timePeriod == "month") return "Last 28 days";
+const timePeriods: TimePeriodType[] = ["hour", "day", "week", "month"];
+
+const displayTimePeriod = (timePeriod: TimePeriod) => {
+  if (timePeriod.type === "hour") return "Last hour";
+  else if (timePeriod.type === "day") return "Last 24 hours";
+  else if (timePeriod.type === "week") return "Last 7 days";
+  else if (timePeriod.type === "month") return "Last 28 days";
+  else
+    return `Custom: ${moment(timePeriod.startTime).format("YYYY-MM-DD")} -> ${moment(
+      timePeriod.endTime
+    ).format("YYYY-MM-DD")}`;
 };
 
-export const TimePicker = ({
-  timePeriod,
-  setTimePeriod,
-}: {
-  timePeriod: string;
-  setTimePeriod: (timePeriod: string) => void;
-}) => {
+export interface TimePeriod {
+  type: TimePeriodType;
+  startTime?: moment.Moment;
+  endTime?: moment.Moment;
+}
+
+interface TimePickerProps {
+  timePeriod: TimePeriod;
+  setTimePeriod: (timePeriod: TimePeriod) => void;
+}
+
+const DateRangePicker = dynamic(() => import("./DateRangePicker"), {
+  ssr: false,
+});
+
+export const TimePicker = ({ timePeriod, setTimePeriod }: TimePickerProps) => {
   const [visible, setVisible] = useState(false);
+  const [dateRangePickerVisible, setDateRangePickerVisible] = useState(false);
+
+  useEffect(() => {
+    if (!visible) {
+      setDateRangePickerVisible(false);
+    }
+  }, [visible]);
 
   return (
     <Menu
@@ -28,26 +52,53 @@ export const TimePicker = ({
       align="right"
       classNames="w-40 md:w-auto"
     >
-      {/* <MenuSection>
-        <MenuItem>
-          <div className="w-8"></div>
-          Real time
-        </MenuItem>
-        <MenuDivider />
-      </MenuSection> */}
       <MenuSection>
         {timePeriods.map((tp) => (
           <MenuItem
             key={tp}
             onClick={() => {
-              setTimePeriod(tp);
+              setTimePeriod({ type: tp });
               setVisible(false);
             }}
           >
-            <div className="w-8">{timePeriod === tp && <Tick />}</div>
-            {displayTimePeriod(tp)}
+            <div className="w-8">{timePeriod.type === tp && <Tick />}</div>
+            {displayTimePeriod({ type: tp })}
           </MenuItem>
         ))}
+      </MenuSection>
+      <MenuDivider />
+      <MenuSection>
+        <MenuItem
+          onClick={() => {
+            setDateRangePickerVisible(true);
+          }}
+        >
+          {dateRangePickerVisible ? (
+            <DateRangePicker
+              startTime={timePeriod.startTime}
+              endTime={timePeriod.endTime}
+              onSelect={(start, end) => {
+                setTimePeriod({
+                  type: "custom",
+                  startTime: moment(start),
+                  endTime: moment(end),
+                });
+                setVisible(false);
+              }}
+            />
+          ) : (
+            <>
+              <div className="w-8">
+                {timePeriod.type === "custom" && <Tick />}
+              </div>
+              {timePeriod.type === "custom" ? (
+                displayTimePeriod(timePeriod)
+              ) : (
+                <>Custom</>
+              )}
+            </>
+          )}
+        </MenuItem>
       </MenuSection>
     </Menu>
   );
