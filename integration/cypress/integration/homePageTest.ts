8cb9cdb07f114005825a7c5d5d530c6f56ebb99b
Alex Sparrow <alex@alexsparrow.dev>
2020-08-29 14:39:27 +0100

    Fix tests?

--- integration/cypress/integration/homePageTest.ts
+++ integration/cypress/integration/homePageTest.ts
@@ -6,21 +6,21 @@ describe("The Home Page", () => {
 
   it("shows the right things", () => {
     cy.visit("http://localhost:3000");
-    cy.contains("button", "Sign up free").should("exist");
+    cy.contains("a", "Sign up free").should("exist");
     cy.contains("a", "Dashboard").should("not.exist");
-    cy.contains("button", "Go to app").should("not.exist");
+    cy.contains("a", "Go to app").should("not.exist");
   });
 
   it("can click signup", () => {
     cy.visit("http://localhost:3000");
-    const signUpButton = cy.contains("button", "Sign up free");
+    const signUpButton = cy.contains("a", "Sign up free");
     signUpButton.click();
     cy.url().should("eq", "http://localhost:3000/sign-up");
   });
 
   it("can click live demo", () => {
     cy.visit("http://localhost:3000");
-    const button = cy.contains("button", "Live demo");
+    const button = cy.contains("a", "Live demo");
     button.click();
     cy.url().should("eq", "http://localhost:3000/domain/beampipe.io");
   });
