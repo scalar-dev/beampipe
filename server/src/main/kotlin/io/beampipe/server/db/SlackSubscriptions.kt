c02c1840df56707374eb87c4cb76a8ec46c585d9
Alex Sparrow <alex@alexsparrow.dev>
2020-09-01 13:26:22 +0100

    Slack summary report

--- server/src/main/kotlin/io/beampipe/server/db/SlackSubscriptions.kt
+++ server/src/main/kotlin/io/beampipe/server/db/SlackSubscriptions.kt
@@ -1,10 +1,14 @@
 package io.beampipe.server.db
 
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.`java-time`.timestamp
 
 object SlackSubscriptions : UUIDTable("slack_subscription") {
     val domainId = uuid("domain_id")
     val eventType = text("event_type")
     val channelId = text("channel_id")
     val teamId = text("team_id")
+    val subscriptionType = text("subscription_type")
+    val subscriptionCron = text("subscription_cron")
+    val lastDeliveryTime = timestamp("last_delivery_time").nullable()
 }
