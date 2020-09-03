f2f3f07babf19de08d0a4ced685311e8a22edd35
Alex Sparrow <alex@alexsparrow.dev>
2020-09-03 16:13:33 +0100

    Overhaul slack subscriptions

--- server/src/main/kotlin/io/beampipe/server/slack/SlackAppFactory.kt
+++ server/src/main/kotlin/io/beampipe/server/slack/SlackAppFactory.kt
@@ -1,10 +1,16 @@
 package io.beampipe.server.slack
 
+import com.slack.api.app_backend.interactive_components.response.ActionResponse
 import com.slack.api.bolt.App
 import com.slack.api.bolt.AppConfig
 import com.slack.api.bolt.context.builtin.SlashCommandContext
 import com.slack.api.bolt.request.builtin.SlashCommandRequest
 import com.slack.api.bolt.response.Response
+import com.slack.api.model.block.DividerBlock
+import com.slack.api.model.block.SectionBlock
+import com.slack.api.model.block.composition.MarkdownTextObject
+import com.slack.api.model.block.composition.PlainTextObject
+import com.slack.api.model.block.element.ButtonElement
 import io.beampipe.server.db.Accounts
 import io.beampipe.server.db.Domains
 import io.beampipe.server.db.SlackSubscriptions
@@ -12,6 +18,7 @@ import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Property
 import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -67,6 +74,47 @@ class AppFactory(@Inject val slackNotifier: SlackNotifier) {
         }
     }
 
+    private fun listSubscriptions(parts: List<String>, req: SlashCommandRequest, ctx:SlashCommandContext): Response =
+        transaction {
+            val unsubscribeSections = SlackSubscriptions.select {
+                SlackSubscriptions.channelId eq (req.payload.channelId) and
+                        (SlackSubscriptions.teamId eq req.payload.teamId)
+            }
+                .map {
+                    val text = if (it[SlackSubscriptions.subscriptionType] == "summary") {
+                        "Weekly summary"
+                    } else {
+                        "Events (type = ${it[SlackSubscriptions.eventType]})"
+                    }
+                    SectionBlock.builder()
+                        .text(MarkdownTextObject(
+                            text,
+                            false
+                        ))
+                        .blockId(it[SlackSubscriptions.id].value.toString())
+                        .accessory(ButtonElement.builder()
+                            .text(
+                                PlainTextObject("unsubscribe", false)
+                            )
+                            .actionId("unsubscribe")
+                            .build()
+                        )
+                        .build()
+                }
+
+            ctx.ack(
+                listOf(
+                            SectionBlock.builder()
+                                .text(MarkdownTextObject(
+                                    "Here are your subscriptions for this channel:",
+                                    false
+                                ))
+                                .build(),
+                    DividerBlock.builder().build()
+                ) + unsubscribeSections
+            )
+        }
+
     private fun subscribeSummary(parts: List<String>, req: SlashCommandRequest, ctx: SlashCommandContext): Response =
         transaction {
             val domain = parts[1]
@@ -82,7 +130,7 @@ class AppFactory(@Inject val slackNotifier: SlackNotifier) {
                     SlackSubscriptions.insertAndGetId {
                         it[SlackSubscriptions.domainId] = domainId.value
                         it[subscriptionType] = "summary"
-                        it[subscriptionCron] = "0 18 * *"
+                        it[subscriptionCron] = "0 17 * * 5"
                         it[channelId] = req.payload.channelId
                         it[teamId] = req.payload.teamId
                     }
@@ -93,6 +141,26 @@ class AppFactory(@Inject val slackNotifier: SlackNotifier) {
                 ctx.ack("Domain not found")
             }
     }
+
+    fun helpMessage(ctx: SlashCommandContext) = ctx.ack(
+        listOf(
+            SectionBlock.builder().text(MarkdownTextObject("I didn't understand that. Try:", false)).build(),
+            DividerBlock.builder().build(),
+            SectionBlock.builder().text(MarkdownTextObject("""To subscribe to an event: 
+                            | `/beampipe subscribe <domain> <event_type>`
+                        """.trimMargin(), false)).build(),
+            SectionBlock.builder().text(MarkdownTextObject("""To subscribe to a weekly summary: 
+                            | `/beampipe summary <domain>`
+                        """.trimMargin(), false)).build(),
+            SectionBlock.builder().text(MarkdownTextObject("""To view a list of subscriptions:
+                            | `/beampipe list`
+                        """.trimMargin(), false)).build(),
+            SectionBlock.builder().text(MarkdownTextObject("""To view this help message:
+                            | `/beampipe help`
+                        """.trimMargin(), false)).build()
+        )
+    )
+
     @Singleton
     fun createApp(config: AppConfig?, @Property(name="slack.command.name", defaultValue = "/beampipe") commandName: String): App {
         val app = App(config)
@@ -102,9 +170,31 @@ class AppFactory(@Inject val slackNotifier: SlackNotifier) {
 
             when (parts[0]) {
                 "subscribe" -> subscribe(parts, req, ctx)
+                "list" -> listSubscriptions(parts, req, ctx)
                 "summary" -> subscribeSummary(parts, req, ctx)
-                else -> ctx.ack("Usage: /beampipe subscribe <domain> <event>")
+                "help" -> helpMessage(ctx)
+                else -> helpMessage(ctx)
             }
+        }
+
+        app.blockAction("unsubscribe") { req, context ->
+            val action = req.payload.actions[0]
+
+            val nDeleted = transaction {
+                SlackSubscriptions.deleteWhere {
+                    SlackSubscriptions.id eq UUID.fromString(action.blockId) and
+                            (SlackSubscriptions.teamId eq req.payload.team.id) and
+                            (SlackSubscriptions.channelId eq req.payload.channel.id)
+                }
+            }
+
+            if (nDeleted >= 1) {
+                context.respond(ActionResponse.builder().text("Unsubscribed").build())
+            } else {
+                context.respond("An error occurred")
+            }
+
+            context.ack()
         }
 
         return app
