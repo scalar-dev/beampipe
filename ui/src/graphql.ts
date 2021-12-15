ad1549c6be1c5464b94dee65de8168b9f85f714d
Alex Sparrow <alex@alexsparrow.dev>
2021-12-15 14:20:18 +0000

    Remove authExchange

--- ui/src/graphql.ts
+++ ui/src/graphql.ts
@@ -1,56 +1,9 @@
-import { authExchange } from "@urql/exchange-auth";
 import {
   cacheExchange,
   createClient,
   dedupExchange,
   fetchExchange,
-  Operation,
 } from "urql";
-
-type AuthState = {
-  token?: string;
-};
-
-const addAuthToOperation = ({
-  authState,
-  operation,
-}: {
-  authState: AuthState;
-  operation: Operation<any, any>;
-}) => {
-  // the token isn't in the auth state, return the operation without changes
-  if (!authState || !authState.token) {
-    return operation;
-  }
-
-  // fetchOptions can be a function (See Client API) but you can simplify this based on usage
-  const fetchOptions =
-    typeof operation.context.fetchOptions === "function"
-      ? operation.context.fetchOptions()
-      : operation.context.fetchOptions || {};
-
-  return {
-    ...operation,
-    context: {
-      ...operation.context,
-      fetchOptions: {
-        ...fetchOptions,
-        headers: {
-          ...fetchOptions.headers,
-          Authorization: `Bearer ${authState.token}`,
-        },
-      },
-    },
-  };
-};
-
-const getAuth = async ({ authState }: { authState: AuthState | null }) => {
-  if (authState === null) {
-    return { token: localStorage.getItem("jwt")! };
-  }
-
-  return null;
-};
 
 const graphqlEndpoint = () =>
   window.location.hostname === "app.trawler.dev"
@@ -63,15 +16,5 @@ export const client = createClient({
   //   ? process.env.REACT_APP_GRAPHQL_ENDPOINT!
   //   : graphqlEndpoint(),
   //
-  exchanges: [
-    dedupExchange,
-    cacheExchange,
-    authExchange({
-      addAuthToOperation,
-      getAuth,
-    }),
-    fetchExchange,
-  ],
+  exchanges: [dedupExchange, cacheExchange, fetchExchange],
 });
-
-console.log(client.fetchOptions);
