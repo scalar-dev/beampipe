9478a8539ebcf20aebb694f702e36fdc15674691
Alex Sparrow <alex@alexsparrow.dev>
2021-12-30 22:33:49 +0000

    Add comment and tippy css (#86)
    * Fix
    
    * Fix links
    
    * Add nasty hack
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'tippy.js/dist/tippy.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
