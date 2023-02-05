16e7dd43a26c277cfede85ad6cb712f16dadb018
Alex Sparrow <alex@alexsparrow.dev>
2023-02-05 16:35:04 +0000

    Add link to scalar.dev (#106)
    * Add link to scalar.dev
    
    * Remove unused
    
    * Scalar.dev links
    
    * Format

--- ui/src/components/layout/Layout.tsx
+++ ui/src/components/layout/Layout.tsx
@@ -7,7 +7,6 @@ import { Avatar } from "./Avatar";
 import {
   faGithub,
   faTwitter,
-  faProductHunt,
   faMedium,
 } from "@fortawesome/free-brands-svg-icons";
 import { UserContext } from "../../utils/auth";
@@ -50,7 +49,7 @@ const SocialButtons = () => (
       <FontAwesomeIcon size="lg" className="fill-current" icon={faMedium} />
     </a>
 
-    <a
+    {/* <a
       className="flex items-center text-gray-500 hover:text-gray-700 mr-3"
       href="https://www.producthunt.com/posts/beampipe"
     >
@@ -59,7 +58,7 @@ const SocialButtons = () => (
         className="fill-current"
         icon={faProductHunt}
       />
-    </a>
+    </a> */}
 
     <a
       className="flex items-center text-gray-500 hover:text-gray-700 mr-3"
@@ -182,7 +181,7 @@ export const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
                 </div>
               </div>
 
-              <div className="pr-2">
+              <div className="pr-2 flex flex-row items-center">
                 <IfUserLoggedIn>
                   <div className="p-4 lg:p-0">
                     <Avatar user={user.user!!} />
@@ -208,7 +207,16 @@ export const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
                   </div>
                 </IfAnonymous>
               </div>
-              <SocialButtons />
+              <div className="flex flex-col items-center">
+                <SocialButtons />
+                <a
+                  className={NAV_LINK_CLASS}
+                  href="https://www.scalar.dev"
+                  onClick={() => setMenuVisible((visible) => !visible)}
+                >
+                  scalar.dev
+                </a>
+              </div>
             </div>
           </nav>
         </div>
