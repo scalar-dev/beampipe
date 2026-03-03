73df3ffea48792a1cc3ba04110222074d8ef8fd4
Alex Sparrow <alex@alexsparrow.dev>
2026-03-03 10:58:31 +0800

    feat: web/ -> astro (#120)
    * chore: tidy some stuff
    
    * Port app to vite
    
    * feat: switch to db-ip database
    
    * Fix env loading
    
    * Add gitkeep
    
    * feat: move web/ to astro

---
+++ web/src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    description: z.string(),
+    author: z.string(),
+  }),
+});
+
+export const collections = { posts };
