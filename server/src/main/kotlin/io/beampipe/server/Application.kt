bcc549892f0fd8058116e83e4b98ea0eaba22c18
Alex Sparrow <alex@alexsparrow.dev>
2026-03-09 10:56:53 +0800

    chore: LOG arenas on startup (#139)

--- server/src/main/kotlin/io/beampipe/server/Application.kt
+++ server/src/main/kotlin/io/beampipe/server/Application.kt
@@ -1,7 +1,19 @@
 package io.beampipe.server
 
 import io.micronaut.runtime.Micronaut
+import io.netty.buffer.PooledByteBufAllocator
+import org.slf4j.LoggerFactory
+
+private val LOG = LoggerFactory.getLogger("io.beampipe.server.Application")
 
 fun main(args: Array<String>) {
+    val alloc = PooledByteBufAllocator.DEFAULT
+    LOG.info("Netty direct arenas: {}, heap arenas: {}, chunk size: {}MB, available processors: {}",
+        alloc.metric().numDirectArenas(),
+        alloc.metric().numHeapArenas(),
+        alloc.metric().chunkSize() / (1024 * 1024),
+        Runtime.getRuntime().availableProcessors()
+    )
+
     Micronaut.run(*args)
 }
